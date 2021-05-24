c88d556cdb3c0fe5f299684e6c62f7072cff33f1
cw <789@webuhu.at>
2021-05-24 10:34:35 +0200

    Update rebar3

--- pkg/rebar3.nix
+++ pkg/rebar3.nix
@@ -2,10 +2,10 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "rebar3";
-  version = "3.15.1";
+  version = "3.16.0";
   # How to obtain hash:
   # nix-prefetch-url https://github.com/erlang/rebar3/releases/download/<version>/rebar3
-  hash = "sha256:1kqkx1kz3rygjb6292hpm2q4hjf0dpdyhamnzd8dxk155wk9zn5f";
+  hash = "sha256:1jyknwjl9bb04klhfphs79sc4nj7c2ysx5w9821a2pr5jj7cayrp";
   src = import <nix/fetchurl.nix> {
     url = "https://github.com/erlang/rebar3/releases/download/${version}/rebar3";
     inherit hash;
